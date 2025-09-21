da18a1339f438165f5a61a88ece16f2ff11e906a
Võ Gia Huy <vogiahuy257@gmail.com>
2025-09-21 09:40:02 +0700

    21-09-2025 Refactor MainActivity and Setting class; update layout for chat functionality and settings panel

--- app/src/main/java/com/example/green/MainActivity.java
+++ app/src/main/java/com/example/green/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private LinearLayout btnHome, btnHistory, chatButton,chatContent, btnSetting;
 
